6a490bf918bd761b5b3ecbb216ec0b1fe4987b9d
sowmya-gade <sowmyag92@gmail.com>
2015-12-01 22:59:19 -0500

    Added line number to line tuple

--- readFile.py
+++ readFile.py
@@ -1,10 +1,14 @@
 
 # This file reads the file and returns all non-empty functions
 
+from collections import namedtuple
+
 def parseInputFile(fileName):
 
     # List of program lines
-    allLines = []
+    allLines = namedtuple('Line', 'line number')
+
+    # List of corresponding line numbers
 
     # TODO: Fill up code
 
